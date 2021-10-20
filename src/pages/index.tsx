87b4e21278e25bccc6aa77c25c55b14bcc288601
Isabel Leccese <imleccese@gmail.com>
2021-10-20 08:23:46 -0400

    update home page copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,17 +11,18 @@ export default function Index() {
         <Layout>
         <div className="landing-page-container">
             <div className="text">
-                <p>Hi, I'm Isabel Leccese and this is my personal website</p>
-                <p>In an effort to <a href="https://www.swyx.io/learn-in-public/" target="_blank">learn in public</a> (and because if I only published posted I considered <i>done</i> I would never publish anything at all),
-                I created a <Link to="/digital-garden">digital garden</Link> - a collection of work-in-progress notes. Mostly about software, but I may explore other topics</p>
+                <h1>hi, I'm Isabel Leccese</h1>
+                <p>I love solving problems and consider myself very fortunate to have access to humanity's finest problem-solving platform - the modern web </p>
+                <p>in particular, I enjoy creating delightful user interfaces in ReactJS</p>
                 <br/>
-                <p>I'm a full-stack software engineer and my weapons of choice are usually ReactJS + python or node.js</p>
-                <p>I'm currently working at <a href="https://www.athenahealth.com/" target="_blank">athenahealth</a>, creating a framework for embedded third-party web applications inside of athenaNet (athena's electronic health record)</p>
+                <p>I'm currently working at <a href="https://www.athenahealth.com/" target="_blank">athenahealth</a>, creating a framework for embedding third-party web applications inside of athenaNet (athena's electronic health record)</p>
                 <br/>
-                <p>Outside of tech, I enjoy hiking, rock climbing, and cooking</p>
+                <p>you can check out what I've been learning and thinking about in my <Link to="/digital-garden">digital garden</Link> - a collection of work-in-progress notes</p>
                 <br/>
-                <p>You won't be able to find me on social media :)</p>
-                <p>But you can contact me at isabel@isabel.tech</p>
+                <p>outside of tech, I enjoy hiking, rock climbing, and cooking</p>
+                <br/>
+                <p>you won't be able to find me on social media :)</p>
+                <p>but you can contact me at isabel@isabel.tech</p>
             </div>
             <div className="photo">
                 <StaticImage 
